b4f4e065ca0fc6edffe6817255c49d8a6bc00050
Damiaan Dufaux <damiaan@dpro.be>
2016-10-19 17:58:14 +0200

    calculate operation on two Ints

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet weak var firstField: UITextField!
+	@IBOutlet weak var operatorSwitch: UISegmentedControl!
+	@IBOutlet weak var secondField: UITextField!
+	@IBOutlet weak var resultLabel: UILabel!
+	
+	@IBAction func calculate(_ sender: AnyObject) {
+		if let a = Int(firstField.text!), let b = Int(secondField.text!) {
+			switch operatorSwitch.selectedSegmentIndex {
+			case 0:
+				resultLabel.text = "\(a+b)"
+			case 1:
+				resultLabel.text = "\(a-b)"
+			default:
+				print("unknown operator selected")
+			}
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
 
 }
 
